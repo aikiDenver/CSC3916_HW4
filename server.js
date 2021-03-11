f116704e92afdc9d2c72fbc9c23823ae770d40d5
aikiDenver <1211lovehope@gmail.com>
2021-03-10 22:30:21 -0700

    fourth commit.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var authJwtController = require('./auth_jwt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
 var User = require('./Users');
+var Movie = require('./Movies');
 
 var app = express();
 app.use(cors());
@@ -105,7 +106,11 @@ router.route('/movie')
             res.json({success: true, msg: 'movie saved.'})
 
         })
-    });
+    }
+    );
+
+
+
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
