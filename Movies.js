7426b3a187734c451cf546a6e07429e60e535917
aikiDenver <1211lovehope@gmail.com>
2021-04-12 21:51:52 -0600

    add imageUrl

--- Movies.js
+++ Movies.js
@@ -19,7 +19,7 @@ var MovieSchema = new Schema({
     year: {type: String, required: true},
     genre: {type: String, emum: ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western"], required: true},
     actors: {type: Array, items: {actorName:String, characterName:String}, required: true, minItems:3},
-    imageUrl: {type: string, required: true}
+    imageUrl: {type: String, required: true}
     //movie_id:{type:Schema.Types.ObjectId}
     //id:{type: String, required: true}
 });
